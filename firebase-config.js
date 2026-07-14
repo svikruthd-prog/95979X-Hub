02fc8cc90389936f882c9b5942c4f02f798cf1f5
svikruthd-prog <svikruthd@gmail.com>
2026-07-14 17:40:54 -0400

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+
+apiKey:"YOUR_API_KEY",
+
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+
+projectId:"YOUR_PROJECT_ID",
+
+storageBucket:"YOUR_PROJECT.appspot.com",
+
+messagingSenderId:"YOUR_ID",
+
+appId:"YOUR_APP_ID"
+
+};
